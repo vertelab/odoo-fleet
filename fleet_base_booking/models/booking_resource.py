65985ef9130838620bc396cea8135cd3884896d0
iammiracle <miracleaayodele@gmail.com>
2025-09-26 16:52:40 +0200

    book fleet as a resource

--- fleet_base_booking/models/booking_resource.py
+++ fleet_base_booking/models/booking_resource.py
@@ -3,5 +3,4 @@ from odoo import fields, models, api
 class BookingResource(models.Model):
     _inherit = 'booking.resource'
 
-    # this should be one2one
     fleet_vehicle_ids = fields.One2many('fleet.vehicle', 'booking_resource_id', string='Fleet Vehicle')
